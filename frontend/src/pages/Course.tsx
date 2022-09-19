298ba0b7c5ee3e9d01fde68ee311a0f29f0491b0
HYUNGJIN <accent680@naver.com>
2022-09-19 16:47:58 +0900

    Coures.tsx에 네이버 지도 구현

--- frontend/src/pages/Course.tsx
+++ frontend/src/pages/Course.tsx
@@ -1,15 +1,58 @@
 import * as React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Dimensions} from 'react-native';
+import NaverMapView, {
+  Align,
+  Circle,
+  Marker,
+  Path,
+  Polygon,
+  Polyline,
+} from 'react-native-nmap';
 
-function Preference({navigation}) {
+function Course() {
+  const P0 = {latitude: 37.53698, longitude: 127.0017};
+  const P1 = {latitude: 37.53154, longitude: 127.007};
+  const P2 = {latitude: 37.55392, longitude: 126.9767};
   return (
-    <View>
-      <Text>지도입니다.</Text>
-      <Text>음식정정보입니다.</Text>
-      <Text>예상가격</Text>
-      <Text>코스시작</Text>
-    </View>
+    <NaverMapView
+      style={{width: '100%', height: '100%'}}
+      showsMyLocationButton={true}
+      center={{...P0, zoom: 16}}
+      // onTouch={e => console.warn('onTouch', JSON.stringify(e.nativeEvent))}
+      onCameraChange={e => console.warn('onCameraChange', JSON.stringify(e))}
+      onMapClick={e => console.warn('onMapClick', JSON.stringify(e))}>
+      <Marker coordinate={P0} onClick={() => console.warn('onClick! p0')} />
+      <Marker
+        coordinate={P1}
+        pinColor="blue"
+        onClick={() => console.warn('onClick! p1')}
+      />
+      <Marker
+        coordinate={P2}
+        pinColor="red"
+        onClick={() => console.warn('onClick! p2')}
+      />
+      <Path
+        coordinates={[P0, P1]}
+        onClick={() => console.warn('onClick! path')}
+        width={10}
+      />
+      <Polyline
+        coordinates={[P1, P2]}
+        onClick={() => console.warn('onClick! polyline')}
+      />
+      <Circle
+        coordinate={P0}
+        color={'rgba(255,0,0,0.3)'}
+        radius={200}
+        onClick={() => console.warn('onClick! circle')}
+      />
+      <Polygon
+        coordinates={[P0, P1, P2]}
+        color={'rgba(0, 0, 0, 0.5)'}
+        onClick={() => console.warn('onClick! polygon')}
+      />
+    </NaverMapView>
   );
 }
-
-export default Preference;
+export default Course;
