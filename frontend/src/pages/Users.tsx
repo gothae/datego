27dee6fb6a1ce584b3dd502d6d7165f19a468cc2
HYUNGJIN <accent680@naver.com>
2022-09-22 12:05:23 +0900

    feat_FE_LoginModal

--- frontend/src/pages/Users.tsx
+++ frontend/src/pages/Users.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import {View, Text} from 'react-native';
 
-function SelectDong() {
+function Users() {
   return (
     <View>
-      <Text>DONG정보입력해주세요</Text>
+      <Text>user정보입력해주세요</Text>
     </View>
   );
 }
 
-export default SelectDong;
+export default Users;
