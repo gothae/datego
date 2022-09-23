6f2cf4efa9183655501006e0ce9650de74463151
Hong Seok In <hrpt8@naver.com>
2022-09-23 11:04:52 +0900

    fix/BE_ERROR 레포지토리 오류

--- backend/src/main/java/com/example/datego/repository/CategoryDetailRepository.java
+++ backend/src/main/java/com/example/datego/repository/CategoryDetailRepository.java
@@ -2,6 +2,8 @@ package com.example.datego.repository;
 
 import com.example.datego.vo.entity.CategoryDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryDetailRepository extends JpaRepository<CategoryDetail, Integer> {
 }
--- backend/src/main/java/com/example/datego/repository/PhotoRepository.java
+++ backend/src/main/java/com/example/datego/repository/PhotoRepository.java
@@ -2,7 +2,9 @@ package com.example.datego.repository;
 
 import com.example.datego.vo.entity.Photo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PhotoRepository extends JpaRepository<Photo, Integer> {
 
 
