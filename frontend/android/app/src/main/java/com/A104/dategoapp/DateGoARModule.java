d9b68c1c725578c4e100839c31fdb582c6c5a017
허상 <daijang35@naver.com>
2022-09-28 09:42:31 +0900

    feat/FE java ar모듈 삭제

--- frontend/android/app/src/main/java/com/A104/dategoapp/DateGoARModule.java
+++
@@ -1,9 +0,0 @@
-package com.A104.dategoapp;
-
-import com.facebook.react.bridge.ReactContextBaseJavaModule;
-
-public class ARModule extends ReactContextBaseJavaModule{
-    ARModule(ReactApplicationContext context){
-        super(context);
-    }
-}
