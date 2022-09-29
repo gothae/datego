49e0ad44e71109237355c9941d60b7a23e163af3
유현욱 <hyundnr1126@naver.com>
2022-09-29 15:41:31 +0900

    feat/FE_Course 코스 변경 기능 추가

--- frontend/src/pages/SpotItem.tsx
+++ frontend/src/pages/SpotItem.tsx
@@ -4,42 +4,68 @@ import { Button } from "@react-native-material/core";
 import { Item, styles } from './ChangeSpot';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { ParamListBase } from '@react-navigation/native';
+import { useSelector } from 'react-redux';
+import { RootState } from '../store/reducer';
+import { useAppDispatch } from '../store';
+import storeSlice from '../slices/stores';
 // type ChangeSpotProps = NativeStackScreenProps<ParamListBase, 'ChangeSpot'>
 type Props = {
   item: Item
   navigation : any
 }
-function SpotItem({ item, navigation } : Props) {
+function SpotItem({ item, navigation }: Props) {
+  const stores: any = useSelector((state: RootState) => state.stores).stores;
+  const storeindex: any = useSelector((state: RootState) => state.stores).storeindex;
+  const dispatch = useAppDispatch();
+  function dispatchCourse(c: any) {
+    dispatch(
+      storeSlice.actions.setstore({
+        stores: c,
+        storeindex: storeindex
+      }),
+    );
+  }
+  const replaceSelectedElement = (arr: any[], selectedIndex: number, newElement: any): any[] => {
+    return arr.map((element: any, index: number) => {
+      if (index === selectedIndex) {
+        return newElement;
+      }
+      return element;
+    });
+  };
   return (
     <View>
       <Pressable style={styles.storeList}
   
         onPress={() => {
           if (!navigation) return;
-      navigation.navigate('DetailSpot', {});
+      navigation.navigate('DetailSpot',{spotId: item.id});
     }}
     >
     <View style={{flex:4}}>
-        <Image style={styles.imageBox} source={{uri: item.thumb}}></Image>
+        <Image style={styles.imageBox} source={{uri: item.image}}></Image>
       </View>
       <View style={{flex: 8, justifyContent:'space-between'}}>
-        <Text style={{ fontSize: 24, fontWeight: 'bold', marginTop: 8 }}>{item.name}</Text>
+        <Text style={{ fontSize: 24, fontWeight: 'bold', marginTop: 8, color:'#000000' }}>{item.name}</Text>
         <View style={{ alignItems:'flex-end',  marginBottom:8, marginRight:8}}>
-          <Button title='변경' color={'#FFA856'}
+            <Button title='변경' color={'#FFA856'}
               titleStyle={{
                 color: "white",
                 fontSize: 14,
-                fontWeight:'bold'
-            }}
-            style={{
-              borderRadius: 60,
-              height: 24,
-              justifyContent:'center'
-            }}
+                fontWeight: 'bold'
+              }}
+              style={{
+                borderRadius: 60,
+                height: 24,
+                justifyContent: 'center'
+              }}
               onPress={() => {
                 if (!navigation) return;
-              navigation.navigate('Course', {});
-            }}
+                const newCourse = replaceSelectedElement(stores, storeindex, item);
+                dispatchCourse(newCourse)
+                // console.log({ 새로운코스: newCourse })
+                navigation.navigate('Course', {});
+              }}
           ></Button>
           
           </View>
