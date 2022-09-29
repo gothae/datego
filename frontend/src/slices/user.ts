e6547627b78a794a6b39330d2946421dd9851e3d
HYUNGJIN <accent680@naver.com>
2022-09-29 09:11:47 +0900

    design_FE_preference

--- frontend/src/slices/user.ts
+++ frontend/src/slices/user.ts
@@ -6,6 +6,7 @@ const initialState = {
   code: 0,
   domain: '',
   id: 0,
+  preference: [],
 };
 const userSlice = createSlice({
   name: 'user',
@@ -16,21 +17,24 @@ const userSlice = createSlice({
       state.accessToken = action.payload.accessToken;
       state.code = action.payload.code;
       state.domain = action.payload.domain;
-      state.id = action.payload.id
+      state.id = action.payload.id;
     },
     logoutUser(state, action) {
       state.email = action.payload.email;
       state.accessToken = action.payload.accessToken;
       state.code = action.payload.code;
       state.domain = action.payload.domain;
-      state.id = action.payload.id
+      state.id = action.payload.id;
     },
     deleteUser(state, action) {
       state.email = action.payload.email;
       state.accessToken = action.payload.accessToken;
       state.code = action.payload.code;
       state.domain = action.payload.domain;
-      state.id = action.payload.id
+      state.id = action.payload.id;
+    },
+    setPreference(state, action) {
+      state.preference = action.payload.preference;
     },
   },
   extraReducers: builder => {},
