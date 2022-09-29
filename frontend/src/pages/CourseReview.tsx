01e40ffbd25c16c0c6e176e25acc6770d045d359
Hong Seok In <hrpt8@naver.com>
2022-09-29 15:44:05 +0900

    feat_FE_리뷰작성페이지 초안

---
+++ frontend/src/pages/CourseReview.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import {View, Text, Image, ScrollView, StyleSheet} from 'react-native';
+import {Button} from '@react-native-material/core';
+
+function CourseReview({navigation, route}: props){
+    const spotId =
+}
