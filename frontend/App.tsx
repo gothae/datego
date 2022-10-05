bad24b1e330f566896b0a5f30a45e40a3b8f516c
kotaehee <koxogml@naver.com>
2022-10-05 14:43:13 +0900

    fix: ignoreAllLogs()

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -7,6 +7,14 @@ import {
 import AppInner from './AppInner';
 import store from './src/store';
 import {Provider} from 'react-redux';
+import {LogBox} from 'react-native';
+// LogBox.ignoreLogs([
+//   'onCameraChange',
+//   'Cannot connect to Metro',
+//   'ComponentWillMount has beeen renamed, and is not recommended for use.',
+//   'source.uri should not be an empty string',
+// ]);
+LogBox.ignoreAllLogs();
 function App() {
   return (
     <Provider store={store}>
--- frontend/src/pages/CourseIng.tsx
+++ frontend/src/pages/CourseIng.tsx
@@ -385,7 +385,7 @@ function CourseIng({navigation}) {
             flex: 5,
           }}>
           <View style={{flex: 1, alignItems: 'center'}}>
-            <View style={{width: '80%', flex: 5}}>
+            <View style={{width: '90%', flex: 5}}>
               <ImageBackground
                 resizeMode="contain"
                 source={{uri: checkImg}}
@@ -509,6 +509,7 @@ function CourseIng({navigation}) {
                 </View>
               </ImageBackground>
             </View>
+            {/* 퀘스트 사진찍기 */}
             <View
               style={{
                 flex: 2,
@@ -527,7 +528,7 @@ function CourseIng({navigation}) {
               <Button
                 style={
                   dist > 100
-                    ? {flex: 2, backgroundColor: 'white'}
+                    ? {flex: 3, backgroundColor: 'white',}
                     : {display: 'none'}
                 }
                 titleStyle={{color: 'orange'}}
